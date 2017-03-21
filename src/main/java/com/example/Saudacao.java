69b13f95085a18ccc50e95a4811cb65a5b081a1e
ls <ls>
2017-03-20 21:27:17 -0300

    segunda rodada

---
+++ src/main/java/com/example/Saudacao.java
@@ -0,0 +1,28 @@
+package com.example;
+
+public class Saudacao {
+
+    private long id;
+    private String conteudo;
+
+    public Saudacao(long incrementAndGet, String format) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+}
